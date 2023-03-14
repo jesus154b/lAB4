5d088c39c077af776930c43cef7a0d27eb2deee4
Jesus Oviedo <jaoviedo@cs.ucsb.edu>
2023-03-14 03:41:20 -0700

    FIFO pasing local TB

--- ucsbece154b_fifo.sv
+++ ucsbece154b_fifo.sv
@@ -88,7 +88,6 @@ module ucsbece154b_fifo #(
                 tail_ptr_d = tail_ptr_d + 1;
                 data_count_d = data_count_d + 1'b1;
             end
-
             
             valid_d = 1'b1;
 
@@ -104,11 +103,11 @@ module ucsbece154b_fifo #(
         if (push_en && pop_en) begin
             // $display("Push+pop, head: %d, tail: %d.", head_ptr_d, tail_ptr_d);
 
-            if((data_count_d == (NR_ENTRIES - 1))) begin // Push after pop, when full
+            if((head_ptr_d == tail_ptr_d) &&(data_count_d == (NR_ENTRIES - 1))) begin // Push after pop, when full
                 $display("Push after pop, when full, head: %d, tail: %d.", head_ptr_d, tail_ptr_d);
                 full_d = 1'b1;
             end
-            if((data_count_d == (0))) begin // Push after pop, when empty
+            if((head_ptr_d == tail_ptr_d) &&(data_count_d == (0))) begin // Push after pop, when empty
                 $display("Push after pop, when empty, head: %d, tail: %d.", head_ptr_d, tail_ptr_d);
                 valid_d = 1'b0; // We are now empty
             end 
@@ -126,6 +125,7 @@ module ucsbece154b_fifo #(
                 else begin  
                     tail_ptr_d = tail_ptr_d + 1;
                 end
+
                 data_count_d = data_count_d;
             end
         end
