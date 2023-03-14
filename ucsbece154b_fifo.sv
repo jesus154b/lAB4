4ebee8757bebcee85b51a1c21fe95d8a4986fd7b
Jesus Oviedo <jaoviedo@cs.ucsb.edu>
2023-03-13 19:27:56 -0700

    Changes to fifo

--- ucsbece154b_fifo.sv
+++ ucsbece154b_fifo.sv
@@ -26,13 +26,13 @@ module ucsbece154b_fifo #(
 
     logic [$clog2(NR_ENTRIES) - 1:0] head_ptr_d, head_ptr_q;
     logic [$clog2(NR_ENTRIES) - 1:0] tail_ptr_d, tail_ptr_q; 
-    // logic [$clog2(NR_ENTRIES):0] data_count_d, data_count_q; 
+    logic [$clog2(NR_ENTRIES):0] data_count_d, data_count_q; 
 
     logic push_en, pop_en;        // Write Enable signal generated iff FIFO is not full
     logic full_d, full_q;        // Full signal
     logic valid_d, valid_q;       // Empty signal
 
-    logic [DATA_WIDTH-1:0] out;
+    // logic [DATA_WIDTH-1:0] out;
 
     // Write and Read Enables internal
     assign push_en = push_i && !full_d;
@@ -40,7 +40,7 @@ module ucsbece154b_fifo #(
 
     assign valid_o = valid_q;
     assign full_o = full_q;
-    assign data_o = out;
+    // assign data_o = out;
 
     integer i = 0;
 
@@ -50,7 +50,7 @@ module ucsbece154b_fifo #(
         // registers
         head_ptr_d = head_ptr_q;
         tail_ptr_d = tail_ptr_q;
-        // data_count_d = data_count_q;
+        data_count_d = data_count_q;
         full_d = full_q;
         valid_d = valid_q;
 
@@ -68,6 +68,13 @@ module ucsbece154b_fifo #(
             end
         end
         
+        // Counter logic, if both read and write not change in amount of data
+        if(!push_en && pop_en ) begin // There was a read
+            data_count_d = data_count_d - 1'b1;
+        end
+        else if(push_en && !pop_en) begin // There was a write
+            data_count_d = data_count_d + 1'b1;
+        end
 
     end
 
@@ -78,37 +85,38 @@ module ucsbece154b_fifo #(
         full_q <= full_d;
         valid_q <= valid_d;
 
-        // data_count_q <= data_count_d;
+        data_count_q <= data_count_d;
 
         // handle reset/flush/disable
         if(rst_i) begin
             head_ptr_q <= 0;
             tail_ptr_q <= 0;
-            // data_count_q <= 0;
+            data_count_q <= 0;
             full_q <= 0;
             valid_q <= 0;
             for (i = 0; i < NR_ENTRIES; i++) begin
                 RAM[i] <= 0;
             end
-            out <= 0;
+            data_o <= 0;
         end
         else begin
             if(pop_en) begin
-                out <= RAM[head_ptr_q];
+                $display("Popping, head_ptr: %d.\n", head_ptr_q );
+                data_o <= RAM[head_ptr_q];
                 head_ptr_q <= head_ptr_q + 1;
                 full_q <= 1'b0;
-                if(tail_ptr_q == head_ptr_q) begin
-                    // $display("Empty.\n");
+                if(((tail_ptr_q - head_ptr_q) == 0) && (data_count_q == 0)) begin
+                    $display("Empty, head: %d, tail: %d.\n", head_ptr_q, tail_ptr_q);
                     valid_q <= 1'b0; // We are now empty
                 end
             end
                 
             if(push_en) begin
-                // $display("Pushing, tail_ptr: %d.\n", tail_ptr_q );
+                $display("Pushing, tail_ptr: %d.\n", tail_ptr_q );
                 RAM[tail_ptr_q] <= data_i;
                 tail_ptr_q <= tail_ptr_q + 1;
                 valid_q <= 1'b1;
-                if(tail_ptr_q == (NR_ENTRIES - 1)) begin
+                if((tail_ptr_q - head_ptr_q) == (NR_ENTRIES - 1)) begin
                     // $display("Full.\n");
                    full_q <= 1'b1;
                 end
